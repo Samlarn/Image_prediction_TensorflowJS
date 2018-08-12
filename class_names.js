bfec27ed0c8aaa6dddefe421dd2877c42f3d964d
Nicklas Jonsson <jonsson.nicklas1990@gmail.com>
2018-08-12 23:26:09 +0200

    large first commit, almost done already..

---
+++ class_names.js
@@ -0,0 +1,20 @@
+
+const LANTMAN_CLASSES = {
+    0: 'baguette',
+    1: 'ciabatta',
+    2: 'croissant',
+    3: 'crown',
+    4: 'curved',
+    5: 'donut',
+    6: 'hamburger bun',
+    7: 'hotdog roll',
+    8: 'loaf',
+    9: 'muffins',
+    10: 'panini',
+    11: 'pecan',
+    12: 'retail',
+    13: 'softdough',
+    14: 'straight',
+    15: 'swirl',
+    16: 'traycake'
+};
